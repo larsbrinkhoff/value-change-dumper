490f02c528aa62f653a532bf7c909a5146865feb
Lars Brinkhoff <lars@nocrew.org>
2023-07-27 20:33:59 +0200

    Remove unused test code.

--- vcd.c
+++ vcd.c
@@ -125,19 +125,3 @@ int vcd_variable (const char *name, const char *type, int width)
   vcd_id (variable[index].id, index);
   return index++;
 }
-
-#ifdef TEST
-int main (void)
-{
-  int reg1, wire2;
-
-  reg1 = vcd_variable ("reg1", "reg", 8);
-  wire2 = vcd_variable ("wire2", "wire", 1);
-  vcd_start ("test.vcd", "Testing", "1ns");
-
-  vcd_value (0, reg1, 0);
-  vcd_value (0, wire2, 0);
-  vcd_value (10, reg1, 0x83);
-  vcd_value (20, wire2, 1);
-}
-#endif
